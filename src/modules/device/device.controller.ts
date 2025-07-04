f6e01385ef281a5253db7025cc76be4acb233354
KhaiNguyen <khaitom1911@.dev@gmail.com>
2025-07-04 15:02:31 +0700

    feat: user decorator

--- src/modules/device/device.controller.ts
+++ src/modules/device/device.controller.ts
@@ -1,27 +1,42 @@
-import { Body, Controller, Get, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { DeviceService } from './device.service';
 import type { DeviceDto } from './device.dto';
 import { Public } from '@/common/decorators/auth.decorator';
+import { Roles } from '@/common/decorators/roles.decorator';
+import { ROLE } from '@/common/enum';
 
 @Controller('device')
 export class DeviceController {
   constructor(private readonly deviceService: DeviceService) {}
 
-  @Public()
-  @Post('create')
-  async create(@Body() deviceDto: DeviceDto) {
-    return await this.deviceService.create(deviceDto);
-  }
-
-  @Public()
   @Get('list')
   async list() {
     return await this.deviceService.list();
   }
 
-  @Public()
+  @Roles(ROLE.ADMIN)
+  @Post('create')
+  async create(@Body() deviceDto: DeviceDto) {
+    return await this.deviceService.create(deviceDto);
+  }
+
+  @Roles(ROLE.ADMIN)
   @Put('update')
   async update(@Body() deviceDto: DeviceDto, @Query('id') id: number) {
     return await this.deviceService.update(deviceDto, id);
+  }
+
+  @Roles(ROLE.ADMIN)
+  @Delete('delete')
+  async delete(@Query('id') id: number) {
+    return await this.deviceService.delete(id);
   }
 }
