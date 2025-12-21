f4fc3f94e5fadaa2027ad79edbf569a4ee1c3b62
khainq20 <khainq20@fpt.com>
2025-12-21 21:26:14 +0700

    user management

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,9 +1,15 @@
 import { UserEntity } from '@/database/entities';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { userDto } from './user.dto';
+import {
+  createUserDto,
+  updateUserDto,
+  userDto,
+  userFilterDto,
+} from './user.dto';
 import { RegisterDto } from '../auth/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -20,6 +26,31 @@ export class UserService {
     };
   }
 
+  public async getUserWithFilter(filter: userFilterDto) {
+    let qb = this.userRepository
+      .createQueryBuilder('user')
+      .innerJoinAndSelect('user.role', 'role');
+
+    if (filter.nameFilter) {
+      qb = qb.andWhere('user.name ILIKE :name', {
+        name: `%${filter.nameFilter}%`,
+      });
+    }
+    if (filter.roleIdFilter) {
+      qb = qb.andWhere('user.role.id = :roleId', {
+        roleId: filter.roleIdFilter,
+      });
+    }
+
+    const count = await qb.getCount();
+    qb = qb
+      .orderBy('user.createdAt', 'DESC')
+      .skip((filter.page - 1) * filter.limit)
+      .take(filter.limit);
+
+    return { data: await qb.getMany(), count };
+  }
+
   async getAll(): Promise<userDto[]> {
     const users = await this.userRepository.find({ relations: ['role'] });
 
@@ -30,5 +61,49 @@ export class UserService {
       phone: user.phone,
       roleId: user.role.id,
     }));
+  }
+
+  public async findById(id: number): Promise<UserEntity | null> {
+    return this.userRepository.findOne({ where: { id }, relations: ['role'] });
+  }
+
+  public async UpdateUser(id: number, data: updateUserDto) {
+    const { roleId, ...rest } = data;
+
+    const oldUser = await this.userRepository.findOne({ where: { id } });
+
+    if (!oldUser) {
+      throw new BadRequestException('User not found');
+    }
+
+    const payload = { ...rest, role: { id: roleId } };
+
+    await this.userRepository.update(id, payload);
+    
+    return this.findById(id);
+  }
+
+  public async createUser(data: createUserDto) {
+    const existingUser = await this.userRepository.findOne({
+      where: [{ email: data.email }, { phone: data.phone }],
+    });
+
+    if (existingUser) {
+      throw new BadRequestException(
+        'User with this email or phone already exists',
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const newUser = this.userRepository.create({
+      ...data,
+      password: hashedPassword,
+      role: { id: data.roleId },
+    });
+
+    await this.userRepository.save(newUser);
+
+    return newUser;
   }
 }
