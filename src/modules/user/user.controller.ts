d664c47bec479642e4d264d7fcf03db205237579
Khai Nguyen <104447228+Khainq1911@users.noreply.github.com>
2025-12-21 21:28:14 +0700

    user management (#31)
    Co-authored-by: khainq20 <khainq20@fpt.com>

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterDto } from '../auth/auth.dto';
+import { createUserDto, updateUserDto, userFilterDto } from './user.dto';
+import { Roles } from '@/common/decorators/roles.decorator';
+import { ROLE } from '@/common/enum';
 
 @Controller('user')
 export class UserController {
@@ -11,8 +14,32 @@ export class UserController {
     return await this.userService.getAll();
   }
 
+  @Roles(ROLE.ADMIN)
+  @Post('filter')
+  async getUserWithFilter(@Body() payload: userFilterDto) {
+    return await this.userService.getUserWithFilter(payload);
+  }
+
   @Post()
   async create(@Body() payload: RegisterDto) {
     return await this.userService.create(payload);
+  }
+
+  @Roles(ROLE.ADMIN)
+  @Put('update/:id')
+  async update(@Param('id') id: number, @Body() payload: updateUserDto) {
+    return await this.userService.UpdateUser(id, payload);
+  }
+
+  @Roles(ROLE.ADMIN)
+  @Post('create')
+  async createUser(@Body() payload: createUserDto) {
+    return await this.userService.createUser(payload);
+  }
+
+  @Roles(ROLE.ADMIN)
+  @Get('find/:id')
+  async findById(@Param('id') id: number) {
+    return await this.userService.findById(id);
   }
 }
--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,7 +1,40 @@
+import { IsEmail, IsNumber, IsString } from 'class-validator';
+
 export class userDto {
   id: number;
   name: string;
   email: string;
   phone: string;
+  roleId: number;
+}
+
+export class userFilterDto {
+  nameFilter?: string;
+  roleIdFilter?: number;
+  limit: number = 5;
+  page: number = 1;
+}
+
+export class updateUserDto {
+  name?: string;
+  email?: string;
+  phone?: string;
+  roleId?: number;
+}
+
+export class createUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  phone: string;
+
+  @IsString()
+  password: string;
+
+  @IsNumber()
   roleId: number;
 }
