3dcfd3a453c75ba200a088df18088ec89063798a
khainq20 <khainq20@fpt.com>
2026-01-29 21:09:26 +0700

    fix seed db

--- src/database/seeders/role.seeder.ts
+++ src/database/seeders/role.seeder.ts
@@ -5,10 +5,8 @@ import { RoleEntity } from '../entities/role.entity';
 export default class RoleSeeder implements Seeder {
   async run(dataSource: DataSource): Promise<any> {
     await dataSource.getRepository(RoleEntity).save([
-      { id: 1, name: 'Worker', alias: 'worker' },
-      { id: 2, name: 'Supervisor', alias: 'supervisor' },
-      { id: 3, name: 'Permit Issuer', alias: 'permit_issuer' },
-      { id: 4, name: 'Admin', alias: 'admin' },
+      { id: 2, name: 'Worker', alias: 'worker' },
+      { id: 1, name: 'Admin', alias: 'admin' },
     ]);
     console.log('>>> ROLE SEED');
   }
